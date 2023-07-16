0d983fb4ed7f3378de3beefa3785bb9375f3117d
Vladislav Stroev <137615800+NupaZzz@users.noreply.github.com>
2023-07-16 23:33:13 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,9 @@
-node slave1 {
-       package { 'httpd':
-              ensure => present,
+package { 'apache2':
+       ensure => present,
        }
-       file { 'index.html' :
-              path => '/var/www/html/index.html',
-              content => "<h1>hello there</h1>",
-              require => Package['httpd'],
-              ensure => file,
-              }
-}
+file { 'index.html' :
+       path => '/var/www/html/index.html',
+       content => "<h1>hello there</h1>",
+       require => Package['apache2'],
+       ensure => file,
+       }
