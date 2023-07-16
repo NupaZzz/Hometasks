80ddcfa52e114ab946d1defd8759fbbe109d76b6
Vladislav Stroev <137615800+NupaZzz@users.noreply.github.com>
2023-07-17 01:55:25 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,4 @@
+include wget
 node slave1 {
   package { 'httpd' :
     ensure => present,
@@ -22,25 +23,12 @@ $slave2_packages = ['php','httpd']
 node slave2 {
   packages { $slave2_packages :
     ensure => present,
-    }
-  file { 'index.php' :
-    path => '/var/www/html/index.php
-    content => '<html>
-  <head>
-    <title>Site is running PHP version <?= phpversion(); ?></title>
-  </head>
-  <body>
-    <?php
-      $limit = rand(1, 1000);
-      for ($i=0; $i<$limit; $i++){
-        echo "<p>Hello, world!</p>";
-      }
-    ?>
-  </body>
-</html>',
-    require => Package['httpd','php']
-    ensure => file,
     enable => true,
+    }
+  wget::fetch { "https://raw.githubusercontent.com/NupaZzz/Hometasks/master/01-vagrant/1/index.php" :
+    destination => '/var/www/html',
+    timeout  => 0,
+    verbose  => false,
     }
   }
 
