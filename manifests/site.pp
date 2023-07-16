617c3e4587be94d3823c4855560161e1a092171c
Vladislav Stroev <137615800+NupaZzz@users.noreply.github.com>
2023-07-17 01:45:27 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -17,3 +17,30 @@ node slave1 {
     enable => true,
   }
 }
+
+$slave2_packages = ['php','httpd']
+node slave2 {
+  packages { $slave2_packages :
+    ensure => present,
+    }
+  file { 'index.php' :
+    path => '/var/www/html/index.php
+    content => '<html>
+  <head>
+    <title>Site is running PHP version <?= phpversion(); ?></title>
+  </head>
+  <body>
+    <?php
+      $limit = rand(1, 1000);
+      for ($i=0; $i<$limit; $i++){
+        echo "<p>Hello, world!</p>";
+      }
+    ?>
+  </body>
+</html>',
+    require => Package['httpd','php']
+    ensure => file,
+    enable => true,
+    }
+  }
+
