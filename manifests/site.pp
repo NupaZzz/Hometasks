86768e869c9dc4c5fbd56cdb7c5234200cc37ee5
Vladislav Stroev <137615800+NupaZzz@users.noreply.github.com>
2023-07-16 23:23:24 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,14 +1,3 @@
-node default {
-       mod 'puppetlabs-apache', '10.1.1'
-}
-
 node slave1 {
-       package { 'httpd':
-        ensure => present,
-    }
-    service { 'httpd':
-        ensure => running,
-        enable => true,
-        require => Package['httpd']
-    }
+       class { 'apache': }
 }
