b641144210a025c137b07f9301041f3e78bbf2e0
Vladislav Stroev <137615800+NupaZzz@users.noreply.github.com>
2023-07-17 00:13:17 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,9 @@
 package { 'apache2' :
   ensure => present,
   }
+file { 'index.html' :
+  path => '/var/www/html/index.html',
+  content => '<h1>Hello there</h1>',
+  require => Package['apache2'],
+  ensure => file,
+  }
