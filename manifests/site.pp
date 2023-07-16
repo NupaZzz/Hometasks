1f7399814cf1822d7a85e01d3f0be579445a305f
Vladislav Stroev <137615800+NupaZzz@users.noreply.github.com>
2023-07-17 01:58:58 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -22,7 +22,6 @@ $slave2_packages = ['php','httpd']
 node slave2 {
   package { $slave2_packages :
     ensure => present,
-    enable => true,
     }
   wget::fetch { "https://raw.githubusercontent.com/NupaZzz/Hometasks/master/01-vagrant/1/index.php" :
     destination => '/var/www/html',
