8b289732dcf43ebcad2a408e82c4e3e8ca165ee5
Vladislav Stroev <137615800+NupaZzz@users.noreply.github.com>
2023-07-17 23:42:16 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,22 +1,16 @@
+include wget
 node slave1 {
   package { 'httpd' :
     ensure => present,
     }
-  file { 'index.html' :
-    path => '/var/www/html/index.html',
-    content => '<html>
-  <head>
-    <title>Success!</title>
-  </head>
-  <body>
-    You Vagrantfile is fine if you can see this message.
-  </body>
-</html>',
-    require => Package['httpd'],
-    ensure => file,
-  }
+  wget::fetch { "https://raw.githubusercontent.com/NupaZzz/Hometasks/master/01-vagrant/1/index.html" :
+    destination => '/var/www/html/index.html,
+    timeout => 0,
+    verbose => false,
+    }
+  service { 'httpd' :
+    enable => true,
 }
-include wget
 $slave2_packages = ['php','httpd']
 node slave2 {
   package { $slave2_packages :
