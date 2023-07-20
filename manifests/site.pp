3c049f7747069c33a33807411e3b9ec9f51c67fc
Vladislav Stroev <137615800+NupaZzz@users.noreply.github.com>
2023-07-21 00:14:41 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,9 +5,9 @@ node master {
     manage_repo => true,
     package_source => 'nginx-mainline',
   }
-  nginx::resource::server { "static.local" :
-    listen_port => 8080,
-    proxy => "http://192.168.30.11:80",
+  nginx::resource::location { '/static' :
+    server => 'http://192.168.30.10,
+    proxy => 'http://192.168.30.11,
   }
 }
 node slave1 {
