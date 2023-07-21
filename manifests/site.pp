8268e617146c06bd92ec80a9bbdd915631c89287
Vladislav Stroev <137615800+NupaZzz@users.noreply.github.com>
2023-07-21 03:14:43 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,17 @@
 include wget
 $slave2_packages = ['php','httpd']
-$master_pacakges = ['nginx']
 node master {
-  package { $master_pacakges :
-    ensure => present,
+  class { 'nginx': 
+    manage_repo => true,
+    package_source => 'nginx-mainline',
+  }
+  nginx::resource::server { '192.168.30.10:8080':
+    listen_port => 8080,
+    proxy       => 'http://192.168.30.11:80',
+  }
+  nginx::resource::server { '192.168.30.10:8081':
+    listen_port => 8081,
+    proxy       => 'http://192.168.30.12:80',
   }
 }
 node slave1 {
