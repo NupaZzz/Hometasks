f7301c77b4fcc3c6060ea2bc49cd08edf1fbdd9d
Vladislav Stroev <137615800+NupaZzz@users.noreply.github.com>
2023-07-22 15:36:23 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -46,26 +46,49 @@ node mineserver {
   package { 'java-11-openjdk' :
     ensure => present,
     }
-  file { '/opt/minecraft':
-    ensure => 'directory',
+  file { '/opt/minecraft/server.jar':
+    ensure => 'file',
+    source => 'https://launcher.mojang.com/v1/objects/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
     owner  => 'root',
     group  => 'root',
-    }
-  wget::fetch { "https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar" :
-    destination => '/opt/minecraft/server.jar',
-    timeout => 0,
-    verbose => false,
-    }
-  exec { 'start_minecraft_server':
-    command => 'java -Xmx1G -Xms1G -jar /opt/minecraft/server.jar nogui',
-    user    => 'root',
-    cwd => '/opt/minecraft',
-    path => '/usr/bin:/usr/sbin:/bin:/sbin'
+    mode   => '0644',
   }
-  service { 'minecraft_server':
-    ensure  => 'running',
-    require => [File['/opt/minecraft'], Exec['start_minecraft_server']],
+  user { 'minecraft':
+    ensure => 'present',
+    system => true,
+    shell  => '/bin/false',
+    home   => '/opt/minecraft',
   }
-}
+  file { '/opt/minecraft':
+    ensure => 'directory',
+    owner  => 'minecraft',
+    group  => 'minecraft',
+    mode   => '0755',
+  }
+  file { '/opt/minecraft/server.properties':
+    ensure => 'file',
+    owner  => 'minecraft',
+    group  => 'minecraft',
+    mode   => '0644',
+    content => "server-port=25565\n",
+  }
+  file { '/etc/systemd/system/minecraft.service':
+    ensure => 'file',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    content => "[Unit]\nDescription=Minecraft Server\nAfter=network.target\n\n[Service]\nType=simple\nUser=minecraft\nWorkingDirectory=/opt/minecraft\nExecStart=/usr/bin/java -Xmx1024M -Xms512M -jar server.jar nogui\nExecStop=/usr/bin/screen -p 0 -S minecraft -X eval 'stuff \"say Сервер Minecraft остановлен.\"\\015'\nExecStop=/bin/sleep 5\nExecStop=/usr/bin/screen -p 0 -S minecraft -X eval 'stuff \"stop\\015\"'\nRestartSec=3\nRestart=always\n\n[Install]\nWantedBy=multi-user.target\n",
+  }
+  exec { 'systemctl daemon-reload':
+    command => '/bin/systemctl daemon-reload',
+    path    => '/usr/bin:/usr/sbin/:/bin:/sbin',
+    onlyif  => 'which systemctl',
+  }service { 'minecraft':
+    ensure    => 'running',
+    enable    => true,
+    hasstatus => true,
+    pattern   => 'java.*server.jar',
+  }
+  }
 
  
