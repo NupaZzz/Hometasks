9bfc13e7e508fdb3577bede63262e5b098c58481
Vladislav Stroev <137615800+NupaZzz@users.noreply.github.com>
2023-07-22 13:17:05 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,7 +44,7 @@ node slave2 {
   }
 node mineserver {
   wget::getch { "https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar"
-    destination => "/opt/minecraft/server.jar",
+    destination => '/opt/minecraft/',
     timeout => 0,
     verbose => false,
     }
