c9f8f7acc4f9461acb0c6ab2de358484638f08b0
NupaZzz <Nupalolz@gmail.com>
2023-08-13 23:15:11 +0300

    add

---
+++ 09-docker-compose/jenkins/nexuscreds.groovy
@@ -0,0 +1,18 @@
+import jenkins.model.*
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.common.*
+import com.cloudbees.plugins.credentials.domains.*
+import com.cloudbees.plugins.credentials.impl.*
+
+def credentialsStore = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
+
+def domain = Domain.global()
+def credentials = new UsernamePasswordCredentialsImpl(
+    CredentialsScope.GLOBAL,
+    '2',
+    'nexus-creds',
+    'nexus',
+    'nexus'
+)
+
+credentialsStore.addCredentials(domain, credentials)
