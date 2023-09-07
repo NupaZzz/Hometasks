2ce30c7ff8fbe697cb2b2cdc419df296b047cd53
Vladislav Stroev <Nupalolz@gmail.com>
2023-09-07 19:01:15 +0300

    Managed by Terraform

--- 13-terraform/github.tf
+++ 13-terraform/github.tf
@@ -10,3 +10,16 @@ resource "github_repository_file" "terraform-files" {
   commit_email        = "Nupalolz@gmail.com"
   overwrite_on_create = true
 }
+
+resource "github_repository_file" "terraform-tfvars" {
+  for_each = fileset("${path.module}", "*.tfvars")
+
+  repository          = var.git_repo_url
+  branch              = "master"
+  file                = "13-terraform/${each.value}"
+  content             = file("${path.module}/${each.value}")
+  commit_message      = "Managed by Terraform"
+  commit_author       = "Vladislav Stroev"
+  commit_email        = "Nupalolz@gmail.com"
+  overwrite_on_create = true
+}
